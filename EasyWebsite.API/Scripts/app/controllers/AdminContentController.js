ad74ce928938f6999f840d25249edb8a28c9c0db
Florian <florian.sofianos@gmail.com>
2015-11-01 11:48:21 +1000

    [EasyWebsite] Some more work on admin-content

--- EasyWebsite.API/Scripts/app/controllers/AdminContentController.js
+++ EasyWebsite.API/Scripts/app/controllers/AdminContentController.js
@@ -1,11 +1,18 @@
 ﻿(function () {
     var app = angular.module("myApp");
 
-    var adminContentController = function ($scope) {
+    var adminContentController = function ($scope, $routeParams, $location) {
 
-        
+        var allModules = moduleHelper.getAll();
+
+        // In case we don't have an operator id, redirect to the first one
+        if (!$routeParams.id) {
+            allModules.$promise.then(function () {
+                $location.url('admin-content/' + allModules[0].Id);
+            });
+        }
 
     };
 
-    app.controller("adminContentController", ['$scope', '$location', adminContentController]);
+    app.controller("adminContentController", ['$scope', '$routeParams', '$location', adminContentController]);
 }());
---
+++ EasyWebsite.API/Scripts/app/services/ModuleHelper.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    var module = angular.module("myApp");
+
+    var moduleHelper = function ($resource) {
+
+        var getAll = function () {
+            return $resource('/api/Module/').query();
+        };
+
+        return {
+            getAll: getAll
+        }
+    };
+
+    module.factory("moduleHelper", moduleHelper);
+}());
