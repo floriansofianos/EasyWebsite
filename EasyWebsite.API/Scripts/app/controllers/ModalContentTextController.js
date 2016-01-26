86db43ddd6034b4869c6142dcf1a63d6287ffea5
Florian <florian.sofianos@gmail.com>
2016-01-26 17:52:16 +1000

    [EasyWebsite] ENH: Add color to the text container

--- EasyWebsite.API/Scripts/app/controllers/ModalContentTextController.js
+++ EasyWebsite.API/Scripts/app/controllers/ModalContentTextController.js
@@ -1,16 +1,28 @@
-﻿function () {
+﻿(function () {
     var app = angular.module("myApp");
 
     var modalContentTextController = function ($scope) {
 
+        var parseHTML = function (element) {
+            var container = $.parseHTML(element)[0];
+            $scope.content = container.innerHTML;
+            $scope.backgroundColor = $(container).css("background-color");
+        };
+
         var element = $scope.$parent.element;
 
         if (element.moduleContentTranslations.length > 0) $scope.content = element.moduleContentTranslations[0].content;
-        else $scope.content = '<div>Hello World!!</div>';
+        else $scope.content = '<div class="text-container"><div>Hello World!!</div></div>';
+
+        parseHTML($scope.content);
 
         $scope.save = function () {
-            $scope.$parent.save($scope.content);
+            //In case there is a background
+            var bckColor = $scope.backgroundColor ? 'background-color: ' + $scope.backgroundColor + ';' : ''
+            $scope.$parent.save('<div class="text-container" style="' + bckColor + '">' + $scope.content + '</div>');
         }
+
+        
 
     };
 
