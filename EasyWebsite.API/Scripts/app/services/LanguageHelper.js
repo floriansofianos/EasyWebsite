f8322bdcf65cd6805a6f418630057b07517e47ef
Florian <florian.sofianos@gmail.com>
2016-03-10 23:02:52 +1000

    [EW] ENH: Add LanguageHelper

---
+++ EasyWebsite.API/Scripts/app/services/LanguageHelper.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var module = angular.module("myApp");
+
+    var languageHelper = function () {
+
+        var availableLanguages = [
+            {
+                label: 'LANGUAGE_FRENCH',
+                code: 'fr'
+            },
+            {
+                label: 'LANGUAGE_ENGLISH',
+                code: 'en'
+            }
+        ];
+
+        return {
+            availableLanguages: availableLanguages
+        }
+    };
+
+    module.factory("languageHelper", languageHelper);
+}());
