e6e5f70d64e1b4da175d031d6e4cf20205326e0f
Florian <florian.sofianos@fonebox.com.au>
2016-03-17 16:58:05 +1000

    [EW] ENH: Add ModuleContent News

---
+++ EasyWebsite.API/Scripts/app/controllers/ModuleNewsController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    var menuModule = angular.module('myApp');
+
+    var moduleNewsController = function ($scope, languageHelper, newsHelper) {
+
+        $scope.allNews = newsHelper.get($scope.moduleid);
+
+        $scope.allNews.$promise.then(function () {
+            $scope.allNews = _.filter($scope.allNews, function (n) { return n.language == languageHelper.getCurrentLanguage(); });
+            $scope.lastNews = _.last($scope.allNews);
+            $scope.otherNews = _.filter($scope.allNews, function (n) { return n.id != $scope.lastNews.id; });
+        });
+
+    };
+
+    menuModule.controller('moduleNewsController', ['$scope', 'languageHelper', 'newsHelper', moduleNewsController]);
+
+}());
---
+++ EasyWebsite.API/Scripts/app/directives/ewModuleNews.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    var app = angular.module('myApp');
+
+    var ewModuleNews = function () {
+        return {
+            replace: true,
+            scope: {
+                moduleid: '='
+            },
+            templateUrl: '/templates/ew-module-news.html',
+            controller: 'moduleNewsController',
+            link: function (scope, elt, attr) {             
+
+            }
+        }
+    };
+
+    app.directive('ewModuleNews', ewModuleNews)
+
+}());
