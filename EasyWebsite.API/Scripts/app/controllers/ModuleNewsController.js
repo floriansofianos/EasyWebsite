a75a1ed2757c252a08637e8b77414cc2b92fd1f1
Florian <florian.sofianos@gmail.com>
2016-03-30 23:12:16 +1000

    [EW] ENH: Some more work on the news module widget. Now we can click on a title to go to the news

--- EasyWebsite.API/Scripts/app/controllers/ModuleNewsController.js
+++ EasyWebsite.API/Scripts/app/controllers/ModuleNewsController.js
@@ -1,9 +1,18 @@
 ﻿(function () {
     var menuModule = angular.module('myApp');
 
-    var moduleNewsController = function ($scope, languageHelper, newsHelper) {
+    var moduleNewsController = function ($scope, languageHelper, newsHelper, moduleHelper, $location) {
 
         $scope.allNews = newsHelper.get($scope.moduleid);
+        $scope.module = moduleHelper.get($scope.moduleid);
+
+        $scope.goToNewsModule = function (action) {
+            $location.path(action);
+        }
+
+        $scope.goToNews = function (action, newsId) {
+            $location.path(action + '/' + newsId);
+        }
 
         $scope.allNews.$promise.then(function () {
             $scope.allNews = _.filter($scope.allNews, function (n) { return n.language == languageHelper.getCurrentLanguage(); });
@@ -13,6 +22,6 @@
 
     };
 
-    menuModule.controller('moduleNewsController', ['$scope', 'languageHelper', 'newsHelper', moduleNewsController]);
+    menuModule.controller('moduleNewsController', ['$scope', 'languageHelper', 'newsHelper', 'moduleHelper', '$location', moduleNewsController]);
 
 }());
